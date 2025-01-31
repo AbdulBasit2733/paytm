4ed5a84f53f5c513e17963b3c784a0c1b577dc82
Abdul Basit <abdulbasitkhan2733@gmail.com>
2025-01-31 16:15:07 +0530

    Setup For Production

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,42 +3,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL || "http://localhost:5173",
+];
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["POST", "GET", "PUT", "DELETE"],
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
   })
 );
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    process.env.FRONTEND_URL || "http://localhost:5173"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // Preflight request
-  }
-  next();
-});
-
-app.use(cookieParser());
+// Handle Preflight Requests
+app.options("*", cors());
 
 const mainRouter = require("../routes/mainRouter");
-
 app.use("/api/v1", mainRouter);
 
 async function Main() {
@@ -49,7 +34,7 @@ async function Main() {
     .then(() => {
       console.log("DB is Connected");
       app.listen(3001, () => {
-        console.log("App is running");
+        console.log("App is running on port 3001");
       });
     })
     .catch((error) => {
