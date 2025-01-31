6e9dd3aaa3f5fc5036fa9abca9666fb46a561a91
Abdul Basit <abdulbasitkhan2733@gmail.com>
2025-01-31 16:09:37 +0530

    Setup For Production

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
@@ -19,6 +20,20 @@ app.use(
     ],
   })
 );
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    process.env.FRONTEND_URL || "http://localhost:5173"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Preflight request
+  }
+  next();
+});
 
 app.use(cookieParser());
 
